5f92abd1f0621059745b048cfdd02ddb9376bb6d
Tran Minh Canh <shiro.tmc@gmail.com>
2020-04-28 21:23:03 +0700

    Updates

---
+++ precache-manifest.fd6bd5b31da444ac1cd5a2e45e2ff2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43a3027abb8bc80389cca76fafef0fc2",
+    "url": "/BTCN04/index.html"
+  },
+  {
+    "revision": "3b4bebecd7426af84a97",
+    "url": "/BTCN04/static/css/main.b22ea7d7.chunk.css"
+  },
+  {
+    "revision": "02bbfa918117c2731b6e",
+    "url": "/BTCN04/static/js/2.597e5acd.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/BTCN04/static/js/2.597e5acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4bebecd7426af84a97",
+    "url": "/BTCN04/static/js/main.19f2e2c9.chunk.js"
+  },
+  {
+    "revision": "45903997ba0a98566dcb",
+    "url": "/BTCN04/static/js/runtime-main.d2d697e5.js"
+  }
+]);
