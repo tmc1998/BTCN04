45cea54374c823616f0f0027f6c7a8cac8c040a0
Tran Minh Canh <shiro.tmc@gmail.com>
2020-04-28 21:40:39 +0700

    TMC 28/04/2020

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';  
+import './App.css';
+import CovidDashboard from "./components/CovidDashboard";
+
+function App() {
+    return (
+        <CovidDashboard/>
+    );
+}
+
+export default App;
